168ab131e9300c68837444c9f26f20f5d86b046f
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2018-01-12 06:02:41 -0200

    starts chapter 8

---
+++ Chapter07/gateway/src/main/java/springfive/airline/infra/route/SampleRoute.java
@@ -0,0 +1,9 @@
+package springfive.airline.infra.route;
+
+/**
+ * @author claudioed on 08/01/18.
+ * Project gateway
+ */
+public class SampleRoute {
+
+}
---
+++ Chapter08/airline-passengers/src/main/java/springfive/airlinepassengers/AirlinePassengersApplication.java
@@ -0,0 +1,13 @@
+package springfive.airlinepassengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinePassengersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AirlinePassengersApplication.class, args);
+  }
+
+}

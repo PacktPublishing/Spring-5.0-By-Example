fed220aeb438e32492c3a56f72dd4b3896d3083a
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2018-02-13 10:56:51 -0200

    planes without security

--- Chapter08/airline-planes/src/main/java/springfive/airline/airlineplanes/AirlinePlanesApplication.java
+++ Chapter08/airline-planes/src/main/java/springfive/airline/airlineplanes/AirlinePlanesApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableZuulProxy
 @EnableEurekaClient
-@EnableResourceServer
 @SpringBootApplication
 public class AirlinePlanesApplication {
 

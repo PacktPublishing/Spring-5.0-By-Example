3724a5e4e46a84f1df30c246154ddc206ae6c6ae
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2018-02-26 22:57:14 -0300

    flights draft

--- Chapter09/airline-booking/src/main/java/springfive/airline/airlinebooking/infra/oauth/CredentialsProducer.java
+++ Chapter09/airline-booking/src/main/java/springfive/airline/airlinebooking/infra/oauth/CredentialsProducer.java
@@ -15,15 +15,23 @@ public class CredentialsProducer {
 
   private final String planesClientSecret;
 
+  private final String flightsClientId;
+
+  private final String flightsClientSecret;
+
   public CredentialsProducer(
       @Value("${fares.oauth.client_id}") String faresClientId,
       @Value("${fares.oauth.client_secret}") String faresClientSecret,
       @Value("${planes.oauth.client_id}") String planesClientId,
-      @Value("${planes.oauth.client_secret}")String planesClientSecret) {
+      @Value("${planes.oauth.client_secret}") String planesClientSecret,
+      @Value("${flights.oauth.client_id}")String flightsClientId,
+      @Value("${flights.oauth.client_secret}")String flightsClientSecret) {
     this.faresClientId = faresClientId;
     this.faresClientSecret = faresClientSecret;
     this.planesClientId = planesClientId;
     this.planesClientSecret = planesClientSecret;
+    this.flightsClientId = flightsClientId;
+    this.flightsClientSecret = flightsClientSecret;
   }
 
   @Bean("faresCredentials")
@@ -34,6 +42,11 @@ public class CredentialsProducer {
   @Bean("planesCredentials")
   public Credentials planes(){
     return Credentials.builder().clientId(this.planesClientId).clientSecret(this.planesClientSecret).build();
+  }
+
+  @Bean("flightsCredentials")
+  public Credentials flights(){
+    return Credentials.builder().clientId(this.flightsClientId).clientSecret(this.flightsClientSecret).build();
   }
 
 }
